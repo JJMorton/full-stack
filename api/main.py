b790322dcb4ab4c1a400ce75f5cf645c14fc1bdb
JJMorton <jjmorton1636@gmail.com>
2023-02-17 15:32:15 +0000

    Added 'hello' route

--- api/main.py
+++ api/main.py
@@ -1,3 +1,13 @@
 from fastapi import FastAPI
+import datetime
 
 app = FastAPI()
+
+@app.get("/")
+async def homepage():
+	time = datetime.datetime.now()
+	return f"The time is {time}"
+
+@app.get("/hello/{name}")
+async def hello_name(name: str):
+	return f"Hello, {name}!"
