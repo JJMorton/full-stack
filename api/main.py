a588e0e026576269bb45ca0b826cd95fda075eda
JJMorton <jjmorton1636@gmail.com>
2023-02-17 16:08:22 +0000

    Added css and js

--- api/main.py
+++ api/main.py
@@ -1,14 +1,20 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 
 from . import settings
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 async def homepage(request: Request):
 	return settings.TEMPLATES.TemplateResponse("home.html", {"request": request})
 
+@app.get("/hello", response_class=HTMLResponse)
+async def hello(request: Request):
+	return settings.TEMPLATES.TemplateResponse("hello.html", {"request": request, "name": "World"})
+
 @app.get("/hello/{name}", response_class=HTMLResponse)
-async def hello_name(name: str):
-	return f"<p>Hello, {name}!</p>"
+async def hello_name(request: Request, name: str):
+	return settings.TEMPLATES.TemplateResponse("hello.html", {"request": request, "name": name})
