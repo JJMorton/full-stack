79dbb5776dbceca54b65df3ce178d5b698472263
JJMorton <jjmorton1636@gmail.com>
2023-02-17 15:56:24 +0000

    Templated homepage

--- api/main.py
+++ api/main.py
@@ -1,13 +1,14 @@
-from fastapi import FastAPI
-import datetime
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+
+from . import settings
 
 app = FastAPI()
 
 @app.get("/")
-async def homepage():
-	time = datetime.datetime.now()
-	return f"The time is {time}"
+async def homepage(request: Request):
+	return settings.TEMPLATES.TemplateResponse("home.html", {"request": request})
 
-@app.get("/hello/{name}")
+@app.get("/hello/{name}", response_class=HTMLResponse)
 async def hello_name(name: str):
-	return f"Hello, {name}!"
+	return f"<p>Hello, {name}!</p>"
